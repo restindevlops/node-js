96122d8ae0447aa6467035616b25018f3c077886
restindevlops <124300271+restindevlops@users.noreply.github.com>
2023-06-02 15:58:24 +0530

    Printing name on screen: Task 06

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello World"); 
+const http = require('http');
+
+const server = http.createServer((req, res) =>{
+    console.log ("Restin");
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My first Page</title></head>');
+    res.write('<body><h1>Restin</h1></body>')
+    res.write('</html>');
+    res.end();
+});
+
+server.listen(4000);
