c653e376ed3db2bfe342fbf0e0c9b94fb37acc30
restindevlops <124300271+restindevlops@users.noreply.github.com>
2023-06-02 16:49:12 +0530

    Writing and reading from a file: Task 08

--- app.js
+++ app.js
@@ -2,44 +2,53 @@ const http = require('http');
 const fs=require('fs');
 const server = http.createServer((req, res) =>{
     const url=req.url;
-    //console.log(url);
+    const method=req.method;
 
-   
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My first Page</title></head>');
-    res.write('<body><h1>Welcome to my Node Js project</h1></body>')
-    res.write('</html>');
-   // res.end();
-     
-
-     if(url==='/home'){
+    if(url==='/'){
+        fs.readFile("message.txt",{ encoding:'utf-8'}, (err,data)=>{
+            if(err){
+                console.log(err);
+            }
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
-        res.write(`<body>Welcome home</body>`);
+        res.write(`<body>${data}</body>`);
+        res.write('<body><form action ="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form> </body>')
         res.write('</html>');
-         //res.end()
+        return res.end()
+
+        })
     
     }
 
-        else if(url==='/about'){
-            res.write('<html>');
-            res.write('<head><title>Enter Message</title></head>');
-            res.write(`<body>Welcome to About Us page</body>`);
-            res.write('</html>');
-        //  res.end()
-        
-        }
-      else if(url==='/node'){
-            res.write('<html>');
-            res.write('<head><title>Enter Message</title></head>');
-            res.write(`<body> Welcome to my Node Js project</body>`);
-            res.write('</html>');
-         // res.end()
-        
-        }
+    if(url==="/message" && method==="POST"){
+        const body=[];
+        req.on("data",(chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end',()=>{
+            const parsedBody=Buffer.concat(body).toString();
+            const message= parsedBody.split('=')[1];
+           fs.writeFile('message.txt',message,(err)=>{
+            if(err){
+                console.log(err)
+            }
+            res.statusCode=302;
+            res.setHeader('Location','/');
+            return res.end();
+           })
+        });
         
 
+
+    }
+
+//     res.setHeader('Content-Type','text/html');
+//     res.write('<html>');
+//     res.write('<head><title>My first Page</title></head>');
+//     res.write('<body><h1>Hello from the Node.js Server</h1></body>')
+//     res.write('</html>');
+//     res.end();
 });
 
 server.listen(4000);
